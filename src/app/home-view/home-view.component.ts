002f390cf50df962a890ea91876e0a65171417fe
Grégory Houllier <gregory.houllier@gmail.com>
2017-02-28 18:05:50 +0100

    feat(home) Update welcome message

--- src/app/home-view/home-view.component.ts
+++ src/app/home-view/home-view.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home-view',
-  template: `<p>home-view works!</p>`,
+  template: `<h1>{{message}}</h1>`,
   styles: [`
+    h1 {
+      text-align: center;
+    }
   `]
 })
 export class HomeViewComponent {
-
+  message = `Congrats, you're connected to ZetaPush!`;
 }

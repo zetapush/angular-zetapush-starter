9d53ba8c1d90bd6251ca27eb6f3479a025a7cba2
Grégory Houllier <gregory.houllier@gmail.com>
2017-03-08 08:25:43 +0100

    doc(lerna) Add todo elements for futur

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule, HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
+// TODO Externalize via lerna
 import { ZetaPushModule } from './zetapush';
+// TODO Externalize via lerna
 import { CoreModule } from './core';
+// TODO Externalize via lerna
 import { RoleModule } from './role';
+// TODO Externalize via lerna
 import { UserModule } from './user';
 
 import { AppComponent } from './app.component';

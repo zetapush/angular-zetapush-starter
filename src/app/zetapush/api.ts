faa2f19a0a14a720ec25c81878c36e1df5911f04
Grégory Houllier <gregory.houllier@gmail.com>
2017-03-23 18:29:19 +0100

    chore(rxjs) Support configurable observable api methods

--- src/app/zetapush/api.ts
+++ src/app/zetapush/api.ts
@@ -2,15 +2,43 @@ import { NgZone } from '@angular/core';
 
 import { services } from 'zetapush-js';
 
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/publish';
+
 import { ZetaPushClient } from './core';
+
+const toPascalCase = (word = '') => `${word.charAt(0).toUpperCase()}${word.substring(1)}`;
+
+const getExtensionsAndListener = (Class: any, zone: NgZone) => {
+  const filter = (element) => element !== 'constructor';
+  const methods = Object.getOwnPropertyNames(Class.prototype).filter(filter);
+  const extensions = {};
+  const listener = methods.reduce((reducer, method) => {
+    const source = Observable.create((observer) => {
+      reducer[method] = ({ data = {} }) => {
+        console.log(`Api::on${toPascalCase(method)}`, data);
+        zone.run(() => {
+          observer.next(data);
+        });
+      };
+    });
+    const published = source.publish();
+    extensions[`on${toPascalCase(method)}`] = published;
+    published.connect();
+    return reducer;
+  }, {});
+  return { extensions , listener };
+};
 
 export class Api extends services.Macro {
 
 }
 
 export function createApi(client: ZetaPushClient, zone: NgZone, Api) {
+  const { extensions , listener } = getExtensionsAndListener(Api, zone);
   const api = client.createAsyncMacroService({
-    Type: Api
+    Type: Api,
+    listener
   });
   const $publish = api.$publish;
   api.$publish = (method: string, parameters: any, hardFail?: boolean, debug?: number) => new Promise<any>((resolve, reject) => {
@@ -22,5 +50,5 @@ export function createApi(client: ZetaPushClient, zone: NgZone, Api) {
     });
     $publish(method, parameters, hardFail, debug).then(onSuccess, onError);
   });
-  return api;
+  return Object.assign(api, extensions);
 }

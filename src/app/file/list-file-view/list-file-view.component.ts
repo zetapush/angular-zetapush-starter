a652f549135c187a82ba5fd140b30571882adcf7
Grégory Houllier <gregory.houllier@gmail.com>
2017-03-30 11:06:23 +0200

    feat(file) Add basic file explorer

--- src/app/file/list-file-view/list-file-view.component.ts
+++ src/app/file/list-file-view/list-file-view.component.ts
@@ -1,8 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { FileApi } from './../file-api.service';
 
 @Component({
   selector: 'zp-list-file-view',
-  templateUrl: './list-file-view.component.html',
-  styles: []
+  template: `
+    <h1>list-file-view</h1>
+    <h3 [attr.contenteditable]="contenteditable" (blur)="onChangeFolder($event)">{{ folder }}</h3>
+    <pre *ngIf="result" class="Result">{{ result | json }}</pre>
+    <pre *ngIf="errors.length" class="Errors">{{ result | json }}</pre>
+  `,
+  styles: [`
+    :host {
+      padding: 1rem;
+    }
+    h3 {
+      border: 1px dashed blue;
+    }
+    .Result {
+      border: 1px dashed green;
+    }
+    .Errors {
+      border: 1px dashed red;
+    }
+  `]
 })
-export class ListFileViewComponent {}
+export class ListFileViewComponent implements OnInit {
+
+  contenteditable = true;
+  errors: Array<any> = [];
+  folder = '/';
+  result: any = {};
+
+  constructor(private api: FileApi) {
+    window['FileApi'] = api;
+  }
+
+  ngOnInit() {
+    this.getFileEntryList();
+  }
+
+  getFileEntryList() {
+    this.api.getFileEntryList({
+      folder: this.folder
+    }).then((result) => {
+      console.log('ListFileViewComponent::onGetFileEntryList', result);
+      this.result = result;
+    }, (errors) => {
+      console.error('ListFileViewComponent::onGetFileEntryList', errors);
+    });
+  }
+
+  onChangeFolder($event) {
+    console.log('ListFileViewComponent::onChangeFolder', $event);
+    this.folder = $event.target.innerText.trim();
+    this.getFileEntryList();
+  }
+}

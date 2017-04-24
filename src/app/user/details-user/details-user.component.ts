12931c926f0d29b0371b884ddd73eb101c5cab0c
CorentinGilles <corentin.gilles@zetapush.com>
2017-04-24 14:26:50 +0200

    Détails utilisateur + liste users présents dans une organisation

--- src/app/user/details-user/details-user.component.ts
+++ src/app/user/details-user/details-user.component.ts
@@ -7,18 +7,56 @@ import { User } from '../';
   template: `
     <p>details-user</p>
     <div>
-      <div><label>Login: </label><span>{{user?.login}}</span></div>
-      <div><label>Email: </label><span>{{user?.email}}</span></div>
-      <div><label>Firstname: </label><span>{{user?.firstname}}</span></div>
-      <div><label>Lastname: </label><span>{{user?.lastname}}</span></div>
-      <div><zp-user-avatar [avatar]="user?.avatar"></zp-user-avatar></div>
+        <img id="avatar" src="./src/assets/img/avatar.png" width="100" height="100" alt="avatar">
     </div>
+    <ul class="mdc-list mdc-list--two-line">
+      <li class="mdc-list-item">
+        <span class="material-icons mdc-list-item__start-detail" aria-hidden="true">person</span>
+        <span class="mdc-list-item__text">User Name
+            <span class="mdc-list-item__text__secondary">{{user?.login}}</span>
+        </span>
+      </li>
+      <li class="mdc-list-item">
+        <span class="material-icons mdc-list-item__start-detail" aria-hidden="true">email</span>
+        <span class="mdc-list-item__text">E-mail
+            <span class="mdc-list-item__text__secondary">{{user?.email}}</span>
+        </span>
+      </li>
+      <li class="mdc-list-item">
+        <span class="material-icons mdc-list-item__start-detail" aria-hidden="true">filter_1</span>
+        <span class="mdc-list-item__text">First Name
+            <span class="mdc-list-item__text__secondary">{{user?.firstname}}</span>
+        </span>
+      </li>
+       <li class="mdc-list-item">
+        <span class="material-icons mdc-list-item__start-detail" aria-hidden="true">filter_2</span>
+        <span class="mdc-list-item__text">Last Name
+            <span class="mdc-list-item__text__secondary">{{user?.lastname}}</span>
+        </span>
+      </li>
+      <div><zp-user-avatar [avatar]="user?.avatar"></zp-user-avatar></div>
+    </ul>
   `,
   styles: [`
     :host {
       display: block;
       border: 1px dashed #333;
     }
+
+    #avatar{
+      overflow:hidden;
+    -webkit-border-radius:80px;
+    -moz-border-radius:80px;
+    border-radius:80px;
+    width:90px;
+    height:90px;
+    margin-left:45px;
+  }
+
+  .mdc-list-item__text, .mdc-list-item__start-detail{
+    font-weight:500;
+    color: #11213b;
+  }
   `]
 })
 export class DetailsUserComponent {
